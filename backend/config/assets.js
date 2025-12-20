4c0883bdb8d0a6106c36dbd2cb28ca3beafd1814
Rahul Kanadia <huckster@outlook.com>
2025-12-20 11:51:10 +0530

    Update assets.js

--- backend/config/assets.js
+++ backend/config/assets.js
@@ -1,56 +1,56 @@
 export const ASSETS = {
   // --- INDICES ---
   NIFTY: {
-    futures: "NIFTY1!",
-    optionPrefix: "NIFTY",
+    futures: "NSE:NIFTY1!",  // Added NSE:
+    optionPrefix: "NSE:NIFTY", // Added NSE:
     strikeStep: 50,
     strikesEachSide: 10,
     type: "INDEX_TUE", 
     exchange: "NSE"
   },
   BANKNIFTY: {
-    futures: "BANKNIFTY1!",
-    optionPrefix: "BANKNIFTY",
+    futures: "NSE:BANKNIFTY1!",
+    optionPrefix: "NSE:BANKNIFTY",
     strikeStep: 100,
     strikesEachSide: 10,
-    type: "INDEX_TUE", // Weekly Tue
+    type: "INDEX_TUE",
     exchange: "NSE"
   },
   FINNIFTY: {
-    futures: "FINNIFTY1!",
-    optionPrefix: "FINNIFTY",
+    futures: "NSE:FINNIFTY1!",
+    optionPrefix: "NSE:FINNIFTY",
     strikeStep: 50,
     strikesEachSide: 10,
     type: "INDEX_TUE",
     exchange: "NSE"
   },
   MIDCPNIFTY: {
-    futures: "MIDCPNIFTY1!",
-    optionPrefix: "MIDCPNIFTY",
+    futures: "NSE:MIDCPNIFTY1!",
+    optionPrefix: "NSE:MIDCPNIFTY",
     strikeStep: 25,
     strikesEachSide: 10,
     type: "INDEX_TUE",
     exchange: "NSE"
   },
   NIFTYNXT50: {
-    futures: "NIFTYNXT501!",
-    optionPrefix: "NIFTYNXT50",
+    futures: "NSE:NIFTYNXT501!",
+    optionPrefix: "NSE:NIFTYNXT50",
     strikeStep: 100,
     strikesEachSide: 5,
     type: "INDEX_TUE",
     exchange: "NSE"
   },
   SENSEX: {
-    futures: "BSX1!",
-    optionPrefix: "BSX",
+    futures: "BSE:BSX1!", // Added BSE:
+    optionPrefix: "BSE:BSX",
     strikeStep: 100,
     strikesEachSide: 10,
-    type: "INDEX_THU", // Weekly Thu
+    type: "INDEX_THU",
     exchange: "BSE"
   },
   BANKEX: {
-    futures: "BKX1!",
-    optionPrefix: "BANKEX",
+    futures: "BSE:BKX1!",
+    optionPrefix: "BSE:BANKEX",
     strikeStep: 100,
     strikesEachSide: 10,
     type: "INDEX_THU",
@@ -59,48 +59,48 @@ export const ASSETS = {
 
   // --- COMMODITIES (MCX) ---
   CRUDEOIL: {
-    futures: "CRUDEOIL1!",
-    optionPrefix: "CRUDEOIL",
+    futures: "MCX:CRUDEOIL1!", // Added MCX:
+    optionPrefix: "MCX:CRUDEOIL",
     strikeStep: 50,
     strikesEachSide: 8,
     type: "MCX_ENERGY", 
     exchange: "MCX"
   },
   NATURALGAS: {
-    futures: "NATURALGAS1!",
-    optionPrefix: "NATURALGAS",
+    futures: "MCX:NATURALGAS1!",
+    optionPrefix: "MCX:NATURALGAS",
     strikeStep: 5, 
     strikesEachSide: 10,
     type: "MCX_ENERGY",
     exchange: "MCX"
   },
   GOLD: {
-    futures: "GOLD1!",
-    optionPrefix: "GOLD",
+    futures: "MCX:GOLD1!",
+    optionPrefix: "MCX:GOLD",
     strikeStep: 100,
     strikesEachSide: 8,
     type: "MCX_BULLION",
     exchange: "MCX"
   },
   SILVER: {
-    futures: "SILVER1!",
-    optionPrefix: "SILVER",
+    futures: "MCX:SILVER1!",
+    optionPrefix: "MCX:SILVER",
     strikeStep: 100, 
     strikesEachSide: 8,
     type: "MCX_BULLION",
     exchange: "MCX"
   },
   COPPER: {
-    futures: "COPPER1!",
-    optionPrefix: "COPPER",
+    futures: "MCX:COPPER1!",
+    optionPrefix: "MCX:COPPER",
     strikeStep: 2.5, 
     strikesEachSide: 6,
     type: "MCX_METAL",
     exchange: "MCX"
   },
   ZINC: {
-    futures: "ZINC1!",
-    optionPrefix: "ZINC",
+    futures: "MCX:ZINC1!",
+    optionPrefix: "MCX:ZINC",
     strikeStep: 1, 
     strikesEachSide: 6,
     type: "MCX_METAL",
